ab6f0fb670febfd374f468a1685e5c8dd65ddb22
daniswastaken <daniswastaken@gmail.com>
2024-07-23 21:02:50 +0700

    jnyfvjhnnchngtnghvfjfv vj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyautogui, time
 time.sleep(5)
-with open("spam.txt", "r") as f:
+with open("bang.txt", "r") as f:
     for lines in f:
         pyautogui.typewrite(lines)
         pyautogui.press("enter")
